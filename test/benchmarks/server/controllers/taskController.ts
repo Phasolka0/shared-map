9dcfc2d694f0967b0a2f6ab030796490415ee7fc
Phasolka0 <100365746+Phasolka0@users.noreply.github.com>
2024-01-22 02:46:32 +0200

    0.1.1

--- test/benchmarks/server/controllers/taskController.ts
+++ test/benchmarks/server/controllers/taskController.ts
@@ -6,7 +6,7 @@ import {getRandomFloat} from "../../../helpers/utils";
 const MAX_KEYS = 100000;
 const NUMBER_OF_GET_KEYS = 10000;
 const GET_KEYS_POOL_SIZE = 2000;
-const TASKS_POOL_SIZE = 2000;
+const TASKS_POOL_SIZE = 1000;
 const sharedMap = new SharedMap({size: MAX_KEYS});
 const map: Map<number, number> = new Map();
 
@@ -15,9 +15,28 @@ let tasksPool: number[][] = [];
 let getKeysPool: number[][] = [];
 
 function regenerateData() {
-	keysCache = keysCache.map(() => getRandomFloat());
-	tasksPool = new Array(TASKS_POOL_SIZE).fill(null).map(() => new Array(MAX_KEYS).fill(null).map(() => getRandomFloat()));
-	getKeysPool = new Array(GET_KEYS_POOL_SIZE).fill(null).map(() => getRandomKeys(keysCache, NUMBER_OF_GET_KEYS));
+	// Перегенерировать keysCache напрямую без .fill().map()
+	for (let i = 0; i < MAX_KEYS; i++) {
+		keysCache[i] = getRandomFloat();
+	}
+	
+	// Перегенерировать tasksPool
+	for (let i = 0; i < TASKS_POOL_SIZE * 2; i++) {
+		if (!tasksPool[i]) {
+			tasksPool[i] = new Array(MAX_KEYS);
+		}
+		for (let j = 0; j < MAX_KEYS; j++) {
+			tasksPool[i][j] = getRandomFloat();
+		}
+	}
+	
+	// Перегенерировать getKeysPool
+	for (let i = 0; i < GET_KEYS_POOL_SIZE * 2; i++) {
+		if (!getKeysPool[i]) {
+			getKeysPool[i] = new Array(NUMBER_OF_GET_KEYS);
+		}
+		getKeysPool[i] = getRandomKeys(keysCache, NUMBER_OF_GET_KEYS);
+	}
 }
 
 regenerateData(); // Инициализация при старте сервера
@@ -39,7 +58,7 @@ export const handleTaskRequest = async (req: Request, res: Response) => {
 		let globalCounter = 0;
 		let randomKeys = getKeysPool.pop();
 		if (!randomKeys) {
-			return res.status(500).send('No more pre-generated key sets available');
+			return res.status(500).send('No more pre-generated key available');
 		}
 		randomKeys.forEach((key) => {
 			let value = usedMap.get(key);
@@ -51,7 +70,9 @@ export const handleTaskRequest = async (req: Request, res: Response) => {
 	} else if (method === 'clear') {
 		usedMap.clear();
 		res.end();
-		if (classType === 'SharedMap') regenerateData();
+	} else if (method === 'regenerate') {
+		regenerateData()
+		res.end();
 		
 	} else {
 		res.status(400).send('Invalid method');
@@ -59,16 +80,16 @@ export const handleTaskRequest = async (req: Request, res: Response) => {
 };
 
 function getRandomKeys(keysCache: number[], numberOfKeys: number) {
-	let shuffledKeys = [...keysCache];
-	let m = shuffledKeys.length, t, i;
+	const result = new Array(numberOfKeys);
+	const taken = new Array(numberOfKeys);
+	let maxIndex = MAX_KEYS; // Используйте переменную для уменьшения индекса
 	
-	while (m) {
-		i = Math.floor(Math.random() * m--);
-		
-		t = shuffledKeys[m];
-		shuffledKeys[m] = shuffledKeys[i];
-		shuffledKeys[i] = t;
+	for (let i = 0; i < numberOfKeys; i++) {
+		let x = Math.floor(Math.random() * (maxIndex - i));
+		result[i] = keysCache[x in taken ? taken[x] : x];
+		taken[x] = --maxIndex in taken ? taken[maxIndex] : maxIndex;
 	}
 	
-	return shuffledKeys.slice(0, numberOfKeys);
+	return result;
 }
+
